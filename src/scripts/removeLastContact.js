eaf1d156a9445ec46279c6802d0a5f4f3e853d25
MarharytaOsypova <osipovamv03@gmail.com>
2025-02-22 17:15:06 +0200

    nodehm1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,22 @@
-export const removeLastContact = async () => {};
+
+import { readContacts } from './../utils/readContacts.js';
+import { writeContacts } from './../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+    try {
+        const contacts = await readContacts();
+    if (contacts.length === 0) {
+        return;
+    }
+    contacts.pop();
+        await writeContacts(contacts);
+        console.log('Done');
+    } catch (error) {
+        console.error(error.message);
+    }
+
+
+
+};
 
 removeLastContact();
